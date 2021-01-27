046c58fedc22e4d516e08afb4b4ba49f628f242c
João Marcos <joaomarcossm2013@hotmail.com>
2021-01-27 17:24:02 -0300

    Id and Increment auto add in Book

--- src/main/java/com/ifsp/apiloja/model/Book.java
+++ src/main/java/com/ifsp/apiloja/model/Book.java
@@ -3,11 +3,17 @@ package com.ifsp.apiloja.model;
 import java.io.Serializable;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Book implements Serializable {
     
     private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int idbook;
     private String author;
     private String title;
