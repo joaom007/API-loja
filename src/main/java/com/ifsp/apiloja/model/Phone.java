486e75e63b664631e6969cb8658672ceb49a388c
João Marcos <joaomarcossm2013@hotmail.com>
2021-01-27 18:20:19 -0300

    Model Phone created

---
+++ src/main/java/com/ifsp/apiloja/model/Phone.java
@@ -0,0 +1,51 @@
+package com.ifsp.apiloja.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Phone {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idPhone;
+    private String phone;
+    private Customer customer;
+
+
+    public Phone() {
+    }
+
+    public Phone(int idPhone, String phone, Customer customer) {
+        this.idPhone = idPhone;
+        this.phone = phone;
+        this.customer = customer;
+    }
+
+    public int getIdPhone() {
+        return this.idPhone;
+    }
+
+    public void setIdPhone(int idPhone) {
+        this.idPhone = idPhone;
+    }
+
+    public String getPhone() {
+        return this.phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Customer getCustomer() {
+        return this.customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+    
+}
