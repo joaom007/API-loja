5252e8b4089c540523eb9dda9a63bb4f2d11eca1
João Marcos <joaomarcossm2013@hotmail.com>
2021-01-29 17:01:28 -0300

    Crud Completed in Category

--- src/main/java/com/ifsp/apiloja/controller/CategoryController.java
+++ src/main/java/com/ifsp/apiloja/controller/CategoryController.java
@@ -1,5 +1,6 @@
 package com.ifsp.apiloja.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import com.ifsp.apiloja.model.Category;
@@ -7,13 +8,18 @@ import com.ifsp.apiloja.service.CategoryService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
-@RequestMapping(value = "/api/categoria")
+@RequestMapping(path = "/api/categoria")
 public class CategoryController {
 
     @Autowired
@@ -24,34 +30,37 @@ public class CategoryController {
         List<Category> list = service.selectCategory();
         return ResponseEntity.ok().body(list);
     }
-
+    //Select *
+    //Return default cod 200 http
     @GetMapping(value = "/{id}")
     public ResponseEntity<Category> selectCategoryById(@PathVariable Long id) {
         Category obj = service.selectCategoryById(id);
         return ResponseEntity.ok().body(obj);
     }
 
+    //Select by id
+    //Return cod 201 http
+    @PostMapping
+    public ResponseEntity<Category> insert(@RequestBody Category obj) {
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
+    }
 
-    // @Autowired
-    // private CategoryRepository categoryRepository;
+    //Delete by id
+    //Return cod 204 http
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 
-    // @GetMapping
-    // public Iterable<Category> selectCategory() {
-    //     return categoryRepository.findAll();
-    // }
+    //Update by id
+    //Return default cod 200 http
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Category> update(@PathVariable Long id, @RequestBody Category obj) {
+        obj = service.update(id, obj);
+        return ResponseEntity.ok().body(obj);
+    }
 
-    
-    // @PostMapping
-    // public @ResponseBody Category insertCategory(Category cat) {
-    //     // Category cat = new Category(category);
-    //     categoryRepository.save(cat);
-    //     return cat;
-    // }
-
-    // @PutMapping
-    // public Category updateCategory(Category cat) {
-    //     // Category cat = new Category(idCategory,category);
-    //     categoryRepository.save(cat);
-    //     return cat;
-    // }
 }
--- src/main/java/com/ifsp/apiloja/service/CategoryService.java
+++ src/main/java/com/ifsp/apiloja/service/CategoryService.java
@@ -3,10 +3,16 @@ package com.ifsp.apiloja.service;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import com.ifsp.apiloja.model.Category;
 import com.ifsp.apiloja.repository.CategoryRepository;
+import com.ifsp.apiloja.service.exception.DataBaseException;
+import com.ifsp.apiloja.service.exception.ResourceNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -21,6 +27,39 @@ public class CategoryService {
     
     public Category selectCategoryById(Long id) {
       Optional<Category> obj = repository.findById(id);
-      return obj.get();
+      return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
+
+    public Category insert(Category obj) {
+      return repository.save(obj);
+    }
+
+    public void delete(Long id) {
+      try {
+        repository.deleteById(id);
+
+      } catch (EmptyResultDataAccessException e) {
+        throw new ResourceNotFoundException(id);
+        
+      } catch (DataIntegrityViolationException e){
+        throw new DataBaseException(e.getMessage());
+      }
+    }
+
+    public Category update(Long id, Category obj) {
+      try {
+        Category model = repository.getOne(id);
+        updateData(model, obj);
+        return repository.save(model);
+      } catch (EntityNotFoundException e) {
+        throw new ResourceNotFoundException(id);
+      }
+      
+    }
+
+    private void updateData(Category model, Category obj) {
+      model.setCategory(obj.getCategory());
+      
+    }
+
 }
