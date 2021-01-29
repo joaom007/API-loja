749c001778f54c3acf4a8eba3658c8a874e3926c
João Marcos <joaomarcossm2013@hotmail.com>
2021-01-29 20:08:55 -0300

    Data Test ajusted

--- src/main/java/com/ifsp/apiloja/config/TestConfig.java
+++ src/main/java/com/ifsp/apiloja/config/TestConfig.java
@@ -51,43 +51,45 @@ public class TestConfig implements CommandLineRunner{
     @Override
     public void run(String... args) throws Exception {
         
-        Category c1 = new Category(null, "teste1");
-        Category c2 = new Category(null, "teste2");
+        Category c1 = new Category(null, "Romance");
+        Category c2 = new Category(null, "Suspense");
 
         categoryRepository.saveAll(Arrays.asList(c1, c2));
 
-        Book b1 = new Book(null, "maria", "A rede de Alice", "98754879", "Érica", "São Paulo", "2°", "2015", "380", 35.99, "description", c1);
-        Book b2 = new Book(null, "maria", "A rede de Alice", "98754879", "Érica", "São Paulo", "2", "2015", "380", 35.99, "description", c2);
+        Book b1 = new Book(null, "Kate Quinn", "A rede de Alice", "98754879", "Érica", "São Paulo", "2°", "2018", "480", 75.99, "legal", c1);
+        Book b2 = new Book(null, "J. K. Rowling", "Harry Potter e a Criança Amaldiçoada", "98754879", "Rocco", "São Paulo", "1", "2016", "380", 35.99, "description", c2);
 
         bookRepository.saveAll(Arrays.asList(b1, b2));
 
-        Customer ct1 = new Customer(null, "João", "999.888.777-66", "99.888.777-6", "testando@hotmail.com", "password", "ashduasjdas", "25/03/1997");
+        Customer ct1 = new Customer(null, "José Benfica", "999.888.777-66", "99.888.777-6", "testando@hotmail.com", "password", "ashduasjdas", "25/03/1997");
 
-        Customer ct2 = new Customer(null, "João", "111.222.333-44", "11.222.333-4", "teste@teste.com", "457895", "aksdhnsajdh", "25/02/2019");
+        Customer ct2 = new Customer(null, "Luciana da Silva", "111.222.333-44", "11.222.333-4", "teste@teste.com", "457895", "aksdhnsajdh", "13/02/1985");
 
         customerRepository.saveAll(Arrays.asList(ct1, ct2));
 
         Phone ph1 = new Phone(null, "11999998888", ct1);
-        Phone ph2 = new Phone(null, "11999998888", ct1);
-        Phone ph3 = new Phone(null, "11999998888", ct2);
+        Phone ph2 = new Phone(null, "55999998888", ct1);
+        Phone ph3 = new Phone(null, "38999998888", ct2);
 
         phoneRepository.saveAll(Arrays.asList(ph1, ph2, ph3));
 
-        Address ad1 = new Address(null, "rua antônio", "450", "centro", "Tatuí", "SP", "18.999-999", ct2);
-        Address ad2 = new Address(null, "rua antônio", "450", "centro", "Tatuí", "SP", "18.999-999", ct1);
-        Address ad3 = new Address(null, "rua antônio", "450", "centro", "Tatuí", "SP", "18.999-999", ct2);
+        Address ad1 = new Address(null, "rua antônio", "450", "centro", "Tatuí", "SP", "18.111-999", ct2);
+        Address ad2 = new Address(null, "Rua do Cruzeiro", "4450", "centro", "Itapetininga", "SP", "18.999-999", ct1);
+        Address ad3 = new Address(null, "Comendador valadares", "589", "centro", "Ipero", "SP", "15.999-999", ct2);
 
         addressRepository.saveAll(Arrays.asList(ad1,ad2, ad3));
 
-        Order od1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), "cartão", "pago", ct1);
-        Order od2 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), "boleto", "cancelado", ct1);
+        Order od1 = new Order(null, Instant.parse("2021-01-20T19:53:07Z"), "cartão", "pago", ct1);
+        Order od2 = new Order(null, Instant.parse("2021-01-20T19:53:07Z"), "boleto", "cancelado", ct1);
 
         orderRepository.saveAll(Arrays.asList(od1, od2));
 
-        OrderItem oi1 = new OrderItem(od1, b1, 1, b1.getUnitaryValue());
-        OrderItem oi2 = new OrderItem(od1, b2, 1, b2.getUnitaryValue());
+        OrderItem oi1 = new OrderItem(od1, b1, 2, b1.getUnitaryValue());
+        OrderItem oi2 = new OrderItem(od2, b2, 5, b2.getUnitaryValue());
+        OrderItem oi3 = new OrderItem(od1, b2, 3, b2.getUnitaryValue());
+        OrderItem oi4 = new OrderItem(od2, b1, 3, b1.getUnitaryValue());
 
-        orderItemRepository.saveAll(Arrays.asList(oi1, oi2));
+        orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
              
     }
 }
