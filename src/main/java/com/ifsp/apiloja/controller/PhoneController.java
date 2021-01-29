757ff0842d3416b1a8d94e846e7950ff117646b8
João Marcos <joaomarcossm2013@hotmail.com>
2021-01-29 17:32:40 -0300

    Phone Crud implemented

--- src/main/java/com/ifsp/apiloja/controller/PhoneController.java
+++ src/main/java/com/ifsp/apiloja/controller/PhoneController.java
@@ -1,5 +1,6 @@
 package com.ifsp.apiloja.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import com.ifsp.apiloja.model.Phone;
@@ -7,10 +8,15 @@ import com.ifsp.apiloja.service.PhoneService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping(path = "/api/telefone")
@@ -24,10 +30,36 @@ public class PhoneController {
         List<Phone> list = service.selectPhone();
         return ResponseEntity.ok().body(list);
     }
-
+    //Select *
+    //Return default cod 200 http
     @GetMapping(value = "/{id}")
     public ResponseEntity<Phone> selectPhoneById(@PathVariable Long id) {
         Phone obj = service.selectPhoneById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+
+    //Select by id
+    //Return cod 201 http
+    @PostMapping
+    public ResponseEntity<Phone> insert(@RequestBody Phone obj) {
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
+    }
+
+    //Delete by id
+    //Return cod 204 http
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    //Update by id
+    //Return default cod 200 http
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Phone> update(@PathVariable Long id, @RequestBody Phone obj) {
+        obj = service.update(id, obj);
         return ResponseEntity.ok().body(obj);
     }
 
--- src/main/java/com/ifsp/apiloja/service/PhoneService.java
+++ src/main/java/com/ifsp/apiloja/service/PhoneService.java
@@ -3,10 +3,16 @@ package com.ifsp.apiloja.service;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import com.ifsp.apiloja.model.Phone;
 import com.ifsp.apiloja.repository.PhoneRepository;
+import com.ifsp.apiloja.service.exception.DataBaseException;
+import com.ifsp.apiloja.service.exception.ResourceNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -21,6 +27,38 @@ public class PhoneService {
     
     public Phone selectPhoneById(Long id) {
       Optional<Phone> obj = repository.findById(id);
-      return obj.get();
+      return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
+
+    public Phone insert(Phone obj) {
+      return repository.save(obj);
+    }
+
+    public void delete(Long id) {
+      try {
+        repository.deleteById(id);
+
+      } catch (EmptyResultDataAccessException e) {
+        throw new ResourceNotFoundException(id);
+        
+      } catch (DataIntegrityViolationException e){
+        throw new DataBaseException(e.getMessage());
+      }
+    }
+
+    public Phone update(Long id, Phone obj) {
+      try {
+        Phone model = repository.getOne(id);
+        updateData(model, obj);
+        return repository.save(model);
+      } catch (EntityNotFoundException e) {
+        throw new ResourceNotFoundException(id);
+      }
+      
+    }
+
+    private void updateData(Phone model, Phone obj) {
+      model.setPhone(obj.getPhone());
+    }
+
 }
