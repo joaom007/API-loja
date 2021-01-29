af2ab72b9dc9c0af3855dc8177fa7a900e5624e4
João Marcos <joaomarcossm2013@hotmail.com>
2021-01-29 16:55:17 -0300

    Address Crud implemented

--- src/main/java/com/ifsp/apiloja/controller/AddressController.java
+++ src/main/java/com/ifsp/apiloja/controller/AddressController.java
@@ -1,5 +1,6 @@
 package com.ifsp.apiloja.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import com.ifsp.apiloja.model.Address;
@@ -7,10 +8,15 @@ import com.ifsp.apiloja.service.AddressService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping(path = "/api/endereco")
@@ -24,10 +30,36 @@ public class AddressController {
         List<Address> list = service.selectAddress();
         return ResponseEntity.ok().body(list);
     }
-
+    //Select *
+    //Return default cod 200 http
     @GetMapping(value = "/{id}")
     public ResponseEntity<Address> selectAddressById(@PathVariable Long id) {
         Address obj = service.selectAddressById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+
+    //Select by id
+    //Return cod 201 http
+    @PostMapping
+    public ResponseEntity<Address> insert(@RequestBody Address obj) {
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
+    }
+
+    //Delete by id
+    //Return cod 204 http
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    //Update by id
+    //Return default cod 200 http
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Address> update(@PathVariable Long id, @RequestBody Address obj) {
+        obj = service.update(id, obj);
         return ResponseEntity.ok().body(obj);
     }
 
--- src/main/java/com/ifsp/apiloja/service/AddressService.java
+++ src/main/java/com/ifsp/apiloja/service/AddressService.java
@@ -3,10 +3,16 @@ package com.ifsp.apiloja.service;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import com.ifsp.apiloja.model.Address;
 import com.ifsp.apiloja.repository.AddressRepository;
+import com.ifsp.apiloja.service.exception.DataBaseException;
+import com.ifsp.apiloja.service.exception.ResourceNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -21,6 +27,43 @@ public class AddressService {
     
     public Address selectAddressById(Long id) {
       Optional<Address> obj = repository.findById(id);
-      return obj.get();
+      return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
+
+    public Address insert(Address obj) {
+      return repository.save(obj);
+    }
+
+    public void delete(Long id) {
+      try {
+        repository.deleteById(id);
+
+      } catch (EmptyResultDataAccessException e) {
+        throw new ResourceNotFoundException(id);
+        
+      } catch (DataIntegrityViolationException e){
+        throw new DataBaseException(e.getMessage());
+      }
+    }
+
+    public Address update(Long id, Address obj) {
+      try {
+        Address model = repository.getOne(id);
+        updateData(model, obj);
+        return repository.save(model);
+      } catch (EntityNotFoundException e) {
+        throw new ResourceNotFoundException(id);
+      }
+      
+    }
+
+    private void updateData(Address model, Address obj) {
+      model.setStreet(obj.getStreet());
+      model.setNumber(obj.getNumber());
+      model.setDistrict(obj.getDistrict());
+      model.setCity(obj.getCity());
+      model.setState(obj.getState());
+      model.setZipCode(obj.getZipCode());      
+    }
+
 }
