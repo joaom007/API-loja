99fad691cfb5e13b12b7118b075d5eab61b823a2
João Marcos <joaomarcossm2013@hotmail.com>
2021-01-29 10:28:44 -0300

    data test in DB created and API ok

--- src/main/java/com/ifsp/apiloja/config/TestConfig.java
+++ src/main/java/com/ifsp/apiloja/config/TestConfig.java
@@ -1,16 +1,19 @@
 package com.ifsp.apiloja.config;
 
+import java.time.Instant;
 import java.util.Arrays;
 
 import com.ifsp.apiloja.model.Address;
 import com.ifsp.apiloja.model.Book;
 import com.ifsp.apiloja.model.Category;
 import com.ifsp.apiloja.model.Customer;
+import com.ifsp.apiloja.model.Order;
 import com.ifsp.apiloja.model.Phone;
 import com.ifsp.apiloja.repository.AddressRepository;
 import com.ifsp.apiloja.repository.BookRepository;
 import com.ifsp.apiloja.repository.CategoryRepository;
 import com.ifsp.apiloja.repository.CustomerRepository;
+import com.ifsp.apiloja.repository.OrderRepository;
 import com.ifsp.apiloja.repository.PhoneRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -37,33 +40,45 @@ public class TestConfig implements CommandLineRunner{
     @Autowired
     private CustomerRepository customerRepository;
 
+    @Autowired
+    private OrderRepository orderRepository;
+
     @Override
     public void run(String... args) throws Exception {
         
         Category c1 = new Category(null, "teste1");
         Category c2 = new Category(null, "teste2");
 
+        categoryRepository.saveAll(Arrays.asList(c1, c2));
+
         Book b1 = new Book(null, "maria", "A rede de Alice", "98754879", "Érica", "São Paulo", "2°", "2015", "380", 35.99, "description", c1);
         Book b2 = new Book(null, "maria", "A rede de Alice", "98754879", "Érica", "São Paulo", "2", "2015", "380", 35.99, "description", c2);
+
+        bookRepository.saveAll(Arrays.asList(b1, b2));
 
         Customer ct1 = new Customer(null, "João", "999.888.777-66", "99.888.777-6", "testando@hotmail.com", "password", "ashduasjdas", "25/03/1997");
 
         Customer ct2 = new Customer(null, "João", "111.222.333-44", "11.222.333-4", "teste@teste.com", "457895", "aksdhnsajdh", "25/02/2019");
 
+        customerRepository.saveAll(Arrays.asList(ct1, ct2));
+
         Phone ph1 = new Phone(null, "11999998888", ct1);
         Phone ph2 = new Phone(null, "11999998888", ct1);
         Phone ph3 = new Phone(null, "11999998888", ct2);
+
+        phoneRepository.saveAll(Arrays.asList(ph1, ph2, ph3));
 
         Address ad1 = new Address(null, "rua antônio", "450", "centro", "Tatuí", "SP", "18.999-999", ct2);
         Address ad2 = new Address(null, "rua antônio", "450", "centro", "Tatuí", "SP", "18.999-999", ct1);
         Address ad3 = new Address(null, "rua antônio", "450", "centro", "Tatuí", "SP", "18.999-999", ct2);
 
-    
-        categoryRepository.saveAll(Arrays.asList(c1, c2));
-        bookRepository.saveAll(Arrays.asList(b1, b2));
-        customerRepository.saveAll(Arrays.asList(ct1, ct2));
-        phoneRepository.saveAll(Arrays.asList(ph1, ph2, ph3));
         addressRepository.saveAll(Arrays.asList(ad1,ad2, ad3));
+
+        Order od1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), "cartão", "pago", ct1);
+        Order od2 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), "boleto", "cancelado", ct1);
+
+        orderRepository.saveAll(Arrays.asList(od1, od2));
+
              
     }
 }
