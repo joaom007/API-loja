302705301ae53baeadf5d4f18d19f276ac7e838b
João Marcos <joaomarcossm2013@hotmail.com>
2021-01-29 08:14:36 -0300

    category add Relationship with book

--- src/main/java/com/ifsp/apiloja/model/Category.java
+++ src/main/java/com/ifsp/apiloja/model/Category.java
@@ -1,14 +1,21 @@
 package com.ifsp.apiloja.model;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
+@Table(name = "tb_category")
 public class Category implements Serializable{
     
     private static final long serialVersionUID = 1L;
@@ -17,6 +24,10 @@ public class Category implements Serializable{
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String category;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "category")
+    private List<Book> books = new ArrayList<>();
 
     public Category() {
 
@@ -42,6 +53,10 @@ public class Category implements Serializable{
 
     public void setCategory(String category) {
         this.category = category;
+    }
+
+    public List<Book> getBooks() {
+        return this.books;
     }
 
     @Override
