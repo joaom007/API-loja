4a2d096657d70cb197afab8ba650e59ce59e1e6b
João Marcos <joaomarcossm2013@hotmail.com>
2021-02-01 15:21:45 -0300

    home page API ajusted

--- src/main/java/com/ifsp/apiloja/controller/HomeController.java
+++ src/main/java/com/ifsp/apiloja/controller/HomeController.java
@@ -1,13 +1,13 @@
 package com.ifsp.apiloja.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
-    @RequestMapping(value = "/")
-    public String index() {
+    @GetMapping(value = "/index")
+    public String Index() {
         return "index";
     }    
 }
