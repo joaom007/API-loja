135e94be1e29cc9fea64efb755281b55801bb948
João Marcos <joaomarcossm2013@hotmail.com>
2021-02-01 15:03:39 -0300

    pom ajusted and home index created

---
+++ src/main/java/com/ifsp/apiloja/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.ifsp.apiloja.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping(value = "/")
+    public String index() {
+        return "index";
+    }    
+}
